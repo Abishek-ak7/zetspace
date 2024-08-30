6d935ce8b1fbe9e39348d4d6c6091cc9c912e11f
Abishek-7ak <abishekcsecs@gmail.com>
2024-08-30 14:29:53 +0530

    Head is added

---
+++ src/components/Head.js
@@ -0,0 +1,98 @@
+import Avator from '../assets/images/Avatar.png'
+
+export default function Head() {
+  return (
+    <div className="2xl:container m-10">
+        <div className='m-7 md:text-start text-center'>
+        <h1 className="font-bold text-lg">Heading</h1>
+        <h2 className="text-gray-300 text-md">Sub Heading</h2>
+        </div>
+        <div className='flex flex-col md:flex justify-center items-center gap-4'>
+        <div className='md:flex gap-4'>
+            <div className="border-gray-300 border-2 mt-4 w-[90%] rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-gray-300 w-[90%] mt-4 border-2 rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-gray-300 w-[90%] mt-4 border-2 rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-gray-300 w-[90%] mt-4 border-2 rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='md:flex gap-4'>
+            <div className="border-gray-300 border-2 mt-4 w-[90%] rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-gray-300 w-[90%] mt-4 border-2 rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-gray-300 w-[90%] mt-4 border-2 rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="border-gray-300 w-[90%] mt-4 border-2 rounded-md md:w-[31%]">
+                <h1 className="font-bold text-lg m-3 ">"Quote"</h1>
+                <div className='flex m-2'>
+                    <img src={Avator} className='w-[20%]' />
+                    <div className='ml-3' >
+                        <h1>Title</h1>
+                        <h2 className="text-gray-300">Description</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+</div>
+  )
+}
